ecf5b84cd6bcc812f5020839513eba0c9ac3c4bf
Alex Nichol <unixpickle@gmail.com>
2014-05-13 10:30:41 -0400

    implemented physical memcpy with functors :\

---
+++ src/platform/x64/memory/phys-memcpy-x64.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014, Alex Nichol and Alux contributors.
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __PLATFORM_X64_PHYS_MEMCPY_X64_H__
+#define __PLATFORM_X64_PHYS_MEMCPY_X64_H__
+
+#include "scope-scratch-x64.h"
+#include <cstring>
+
+namespace OS {
+
+namespace x64 {
+
+void MemcpyToPhys(PhysAddr phys, VirtAddr virt, size_t size);
+void MemcpyToVirt(VirtAddr virt, PhysAddr phys, size_t size);
+
+template <class F>
+void VirtualizePhysical(PhysAddr phys, size_t size, const F & func) {
+  ScopeScratch scope(phys & ~0xfffL);
+  size_t offset = 0;
+  while (offset < size) {
+    size_t amount = 0x1000 - (phys & 0xfff);
+    if (amount > size) amount = size;
+    func((void *)(scope.GetVirtAddr() + (phys & 0xfff)), offset, amount);
+    offset += amount;
+    if (size > offset) {
+      phys += amount;
+      scope.Reassign(phys);
+    }
+  }
+}
+
+}
+
+}
+
+#endif
+
