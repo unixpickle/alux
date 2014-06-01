a62aae9ca0414c59fb1a7b12ca5d7990eb8ea6fe
Alex Nichol <unixpickle@gmail.com>
2014-06-01 18:33:23 -0400

    started working on scheduler

---
+++ src/arch/general/deadline.hpp
@@ -0,0 +1,11 @@
+#ifndef __GENERAL_DEADLINE_HPP__
+#define __GENERAL_DEADLINE_HPP__
+
+namespace OS {
+
+void DeadlineSet(uint64_t time);
+void DeadlineCancel();
+
+}
+
+#endif
---
+++ src/scheduler/task.hpp
@@ -0,0 +1,71 @@
+#ifndef __SCHEDULER_TASK_HPP__
+#define __SCHEDULER_TASK_HPP__
+
+#include <cstdint>
+#include <common>
+
+namespace OS {
+
+class Thread;
+
+enum KillReason {
+  KillReasonSelf = 0,
+  KillReasonExternal,
+  KillReasonMemory
+};
+
+class Task {
+public:
+  Task * next, * last;
+  
+  Task();
+  virtual ~Task();
+  
+  /**
+   * If the task is not being killed, or if it is held, this will increment the
+   * task's reference count and return true. Otherwise, the reference count
+   * will not be changed and false will be returned.
+   *
+   * @critical
+   */
+  virtual bool Retain();
+  
+  /**
+   * If the task is being killed, this returns false. Otherwise, it increments
+   * the reference count of the task and "holds" the task. Now, the task cannot
+   * be killed until the task is unheld.
+   *
+   * @critical
+   */
+  virtual bool Hold();
+  
+  /**
+   * Unholds the task and then releases it.
+   *
+   * @critical
+   */
+  virtual void Unhold();
+  
+  /**
+   * @critical
+   */
+  virtual void Release();
+  
+protected:
+  // TODO: an ivar should go here for a task address space
+  
+  // thread list
+  uint64_t threadsLock OS_ALIGNED(8);
+  Thread * firstThread;
+  
+  // state information
+  uint64_t stateLock OS_ALIGNED(8);
+  uint64_t refCount;
+  bool isKilled;
+  bool isHeld;
+  uint64_t killReason;
+};
+
+}
+
+#endif
