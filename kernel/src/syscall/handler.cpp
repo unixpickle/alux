2ac33a246f7e087ba4c414d6dc035e63983da465
Alex Nichol <unixpickle@gmail.com>
2014-08-27 21:45:24 -0400

    added handlers and a special message :)

--- kernel/src/syscall/handler.cpp
+++ kernel/src/syscall/handler.cpp
@@ -11,6 +11,9 @@ namespace Alux {
 anarch::SyscallRet SyscallHandler(uint16_t number,
                                   anarch::SyscallArgs & args) {
   AssertCritical();
+  // You might be like "holy crap this is terrible". I'd respond to you as
+  // follows: out of all of the syscall mechanisms I've seen, this is the most
+  // clear, efficient, and unified way to do it.
   switch (number) {
     case 0:
       PrintSyscall(args);
@@ -37,6 +40,24 @@ anarch::SyscallRet SyscallHandler(uint16_t number,
       break;
     case 10:
       return GetNanoTimeSyscall();
+    case 11:
+      return VMReadSyscall(args);
+    case 12:
+      return VMMapSyscall(args);
+    case 13:
+      return VMMapAtSyscall(args);
+    case 14:
+      return VMUnmapSyscall(args);
+    case 15:
+      return VMUnmapAndReserveSyscall(args);
+    case 16:
+      return VMReserveSyscall(args);
+    case 17:
+      return VMReserveAtSyscall(args);
+    case 18:
+      return VMUnreserveSyscall(args);
+    case 19:
+      return VMRereserveSyscall(args);
     default:
       anarch::cerr << "unknown SyscallHandler(" << number << ", ...)"
         << anarch::endl;
