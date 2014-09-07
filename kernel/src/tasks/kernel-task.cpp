4d82d31008229720b69d1af6e28a25ed3df16a3b
Alex Nichol <unixpickle@gmail.com>
2014-09-07 12:06:40 -0400

    removed debug print

--- kernel/src/tasks/kernel-task.cpp
+++ kernel/src/tasks/kernel-task.cpp
@@ -2,20 +2,16 @@
 #include <anarch/critical>
 #include <anarch/api/global-map>
 
-#include <anarch/stream> // TODO: delete this
-
 namespace Alux {
 
 KernelTask * KernelTask::New(Scheduler & s) {
   AssertNoncritical();
   KernelTask * t = new KernelTask(s);
   if (!t) return NULL;
-  anarch::cout << "about to attempt kernel init...";
   if (!t->Init()) {
     delete t;
     return NULL;
   }
-  anarch::cout << "OK\n";
   return t;
 }
 
